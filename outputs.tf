e29cb5593ea69393f59d1b1ebc6a968244eeedc0
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:18:36 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,25 +1,21 @@
-output "logic_app_integration_account_partners" {
-  description = "All logic_app_integration_account_partner resources"
-  value       = azurerm_logic_app_integration_account_partner.logic_app_integration_account_partners
-}
 output "logic_app_integration_account_partners_business_identity" {
-  description = "List of business_identity values across all logic_app_integration_account_partners"
-  value       = [for k, v in azurerm_logic_app_integration_account_partner.logic_app_integration_account_partners : v.business_identity]
+  description = "Map of business_identity values across all logic_app_integration_account_partners, keyed the same as var.logic_app_integration_account_partners"
+  value       = { for k, v in azurerm_logic_app_integration_account_partner.logic_app_integration_account_partners : k => v.business_identity }
 }
 output "logic_app_integration_account_partners_integration_account_name" {
-  description = "List of integration_account_name values across all logic_app_integration_account_partners"
-  value       = [for k, v in azurerm_logic_app_integration_account_partner.logic_app_integration_account_partners : v.integration_account_name]
+  description = "Map of integration_account_name values across all logic_app_integration_account_partners, keyed the same as var.logic_app_integration_account_partners"
+  value       = { for k, v in azurerm_logic_app_integration_account_partner.logic_app_integration_account_partners : k => v.integration_account_name }
 }
 output "logic_app_integration_account_partners_metadata" {
-  description = "List of metadata values across all logic_app_integration_account_partners"
-  value       = [for k, v in azurerm_logic_app_integration_account_partner.logic_app_integration_account_partners : v.metadata]
+  description = "Map of metadata values across all logic_app_integration_account_partners, keyed the same as var.logic_app_integration_account_partners"
+  value       = { for k, v in azurerm_logic_app_integration_account_partner.logic_app_integration_account_partners : k => v.metadata }
 }
 output "logic_app_integration_account_partners_name" {
-  description = "List of name values across all logic_app_integration_account_partners"
-  value       = [for k, v in azurerm_logic_app_integration_account_partner.logic_app_integration_account_partners : v.name]
+  description = "Map of name values across all logic_app_integration_account_partners, keyed the same as var.logic_app_integration_account_partners"
+  value       = { for k, v in azurerm_logic_app_integration_account_partner.logic_app_integration_account_partners : k => v.name }
 }
 output "logic_app_integration_account_partners_resource_group_name" {
-  description = "List of resource_group_name values across all logic_app_integration_account_partners"
-  value       = [for k, v in azurerm_logic_app_integration_account_partner.logic_app_integration_account_partners : v.resource_group_name]
+  description = "Map of resource_group_name values across all logic_app_integration_account_partners, keyed the same as var.logic_app_integration_account_partners"
+  value       = { for k, v in azurerm_logic_app_integration_account_partner.logic_app_integration_account_partners : k => v.resource_group_name }
 }
 
